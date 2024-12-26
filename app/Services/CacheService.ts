107d3b2484b265cb8b77a1eaa2174bf2fc5dd812
Pedro Pastuk <pastukpedro@gmail.com>
2024-12-26 18:36:25 -0300

    refactor: clean up code style in CacheService and MovieService for consistency

--- app/Services/CacheService.ts
+++ app/Services/CacheService.ts
@@ -1,55 +1,51 @@
 interface CacheItem<T> {
-  data: T
-  expiry: number
+  data: T;
+  expiry: number;
 }
 
 export default class CacheService<T> {
-  private static instances: Map<string, CacheService<any>> = new Map()
-  private cache: Map<string, CacheItem<T>> = new Map()
-  private ttl: number
+  private static instances: Map<string, CacheService<any>> = new Map();
+  private cache: Map<string, CacheItem<T>> = new Map();
+  private ttl: number;
 
   private constructor(ttlInSeconds: number = 1500) {
-    this.ttl = ttlInSeconds * 1000
+    this.ttl = ttlInSeconds * 1000;
   }
 
-  public static getInstance<T>(key: string = 'default', ttlInSeconds: number = 1500): CacheService<T> {
+  public static getInstance<T>(
+    key: string = "default",
+    ttlInSeconds: number = 1500
+  ): CacheService<T> {
     if (!this.instances.has(key)) {
-      this.instances.set(key, new CacheService<T>(ttlInSeconds))
+      this.instances.set(key, new CacheService<T>(ttlInSeconds));
     }
-    return this.instances.get(key) as CacheService<T>
+    return this.instances.get(key) as CacheService<T>;
   }
 
   set(key: string, value: T): void {
-    const expiry = Date.now() + this.ttl
-    console.log('Setting cache for key:', key)
-    console.log('Expiry time:', expiry)
+    const expiry = Date.now() + this.ttl;
     this.cache.set(key, {
       data: value,
-      expiry: expiry
-    })
+      expiry: expiry,
+    });
   }
 
   get(key: string): T | null {
-    const item = this.cache.get(key)
+    const item = this.cache.get(key);
 
     if (!item) {
-      console.log('Cache miss - key not found:', key)
-      return null
+      return null;
     }
 
     if (Date.now() > item.expiry) {
-      console.log('Cache expired for key:', key)
-      console.log('Current time:', Date.now())
-      console.log('Expiry time:', item.expiry)
-      this.cache.delete(key)
-      return null
+      this.cache.delete(key);
+      return null;
     }
 
-    console.log('Cache hit - returning data for key:', key)
-    return item.data
+    return item.data;
   }
 
   clear(): void {
-    this.cache.clear()
+    this.cache.clear();
   }
 }
--- app/Services/MovieService.ts
+++ app/Services/MovieService.ts
@@ -1,82 +1,79 @@
-import { ObjectId } from 'mongodb'
-import Database from '@ioc:Zakodium/Mongodb/Database'
-import Movie from 'App/Models/Movie'
-import CacheService from './CacheService'
+import { ObjectId } from "mongodb";
+import Database from "@ioc:Zakodium/Mongodb/Database";
+import Movie from "App/Models/Movie";
+import CacheService from "./CacheService";
 
 export default class MovieService {
-  private searchCache: CacheService<any[]>
+  private searchCache: CacheService<any[]>;
 
   constructor() {
-    this.searchCache = CacheService.getInstance<any[]>('movies', 300)
+    this.searchCache = CacheService.getInstance<any[]>("movies", 300);
   }
 
   private async getCollection() {
-    return await Database.connection().collection('movies')
+    return await Database.connection().collection("movies");
   }
 
   public async listAll() {
-    const collection = await this.getCollection()
-    return await collection.find().toArray()
+    const collection = await this.getCollection();
+    return await collection.find().toArray();
   }
 
   public async search(query: string) {
-    const cacheKey = `search:${query}`
-    const cachedResult = this.searchCache.get(cacheKey)
+    const cacheKey = `search:${query}`;
+    const cachedResult = this.searchCache.get(cacheKey);
 
     if (cachedResult) {
-      console.log('Cache hit for query:', query);
-      console.log('Cached data:', cachedResult);
       return cachedResult;
     }
 
-    console.log('Cache miss for query:', query);
-    const collection = await this.getCollection()
-    const results = await collection.find({
-      $or: [
-        { title: { $regex: query, $options: 'i' } },
-        { genre: { $regex: query, $options: 'i' } }
-      ]
-    }).toArray()
+    const collection = await this.getCollection();
+    const results = await collection
+      .find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { genre: { $regex: query, $options: "i" } },
+        ],
+      })
+      .toArray();
 
-    console.log('Setting cache for query:', query);
-    console.log('Data being cached:', results);
-    this.searchCache.set(cacheKey, results)
+    this.searchCache.set(cacheKey, results);
 
-    return results
+    return results;
   }
 
   private clearSearchCache(): void {
-    this.searchCache.clear()
+    this.searchCache.clear();
   }
 
   public async create(movieData: Partial<Movie>) {
-    const collection = await this.getCollection()
-    const movie = new Movie(movieData)
-    const result = await collection.insertOne(movie)
-    this.clearSearchCache()
-    return { ...movie, _id: result.insertedId }
+    const collection = await this.getCollection();
+    const movie = new Movie(movieData);
+    const result = await collection.insertOne(movie);
+    this.clearSearchCache();
+    return { ...movie, _id: result.insertedId };
   }
 
   public async update(id: string, movieData: Partial<Movie>) {
-    const collection = await this.getCollection()
-    const _id = new ObjectId(id)
+    const collection = await this.getCollection();
+    const _id = new ObjectId(id);
     const result = await collection.findOneAndUpdate(
       { _id },
       { $set: { ...movieData, updatedAt: new Date() } },
-      { returnDocument: 'after' }
-    )
-    this.clearSearchCache()
+      { returnDocument: "after" }
+    );
+    this.clearSearchCache();
     if (!result) {
-      return null
+      return null;
     }
-    return { ...result }
+    return { ...result };
   }
 
   public async delete(id: string) {
-    const collection = await this.getCollection()
-    const _id = new ObjectId(id)
-    const result = await collection.deleteOne({ _id })
-    this.clearSearchCache()
-    return result
+    const collection = await this.getCollection();
+    const _id = new ObjectId(id);
+    const result = await collection.deleteOne({ _id });
+    this.clearSearchCache();
+    return result;
   }
 }
